9f93388efe6abb800c50c2de0033cd627fecd3db
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2026-08-21 21:47:02 -0300

    refactor: add rupture edge to memory handoff

--- src/components/experience/hero/tsukihara-hero.tsx
+++ src/components/experience/hero/tsukihara-hero.tsx
@@ -170,6 +170,9 @@ export function TsukiharaHero({ copy, locale }: TsukiharaHeroProps) {
               <i key={index} data-memory-ash />
             ))}
           </div>
+          <div className="th-memory-rupture" data-memory-rupture>
+            <Image src="/ruptura-separador.png" alt="" fill sizes="110vw" />
+          </div>
         </div>
       </div>
     </section>
