daf5ef332ae8f7c75590b852c8459da130659d1b
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2026-08-23 22:43:32 -0300

    feat: add mother moon asset primitive

---
+++ src/components/experience/mother-moon/mother-moon-asset.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import {
+  motherMoonAssets,
+  type MotherMoonAssetCode,
+} from "@/content/mother-moon";
+
+type MotherMoonAssetProps = {
+  code: MotherMoonAssetCode;
+  className?: string;
+  sizes?: string;
+  priority?: boolean;
+};
+
+export function MotherMoonAsset({
+  code,
+  className,
+  sizes = "100vw",
+  priority = false,
+}: MotherMoonAssetProps) {
+  return (
+    <Image
+      src={motherMoonAssets[code]}
+      alt=""
+      fill
+      sizes={sizes}
+      priority={priority}
+      className={className}
+      aria-hidden="true"
+      data-mm-asset={code}
+    />
+  );
+}
