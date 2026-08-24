10c60df084fa00ca786e12573e82b9f30514726b
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2026-08-23 21:58:38 -0300

    style: load bestiary integrated inspector

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ import "./gameplay-chapter.css";
 import "./gameplay-typography-fix.css";
 import "./companions-chapter.css";
 import "./bestiary-bosses.css";
+import "./bestiary-inspector-refinement.css";
 import "./nine-realms-world.css";
 import "./nine-realms-world-calibration.css";
 import "./nine-realms-world-stability.css";
